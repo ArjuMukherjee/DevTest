1d36267ca6362f76401d9b5f5ac44e38b7f8ec3b
Arju <arju234567@gmail.com>
2024-10-21 19:31:45 +0530

    vercel json

--- manage.py
+++ manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from DevTest.wsgi import application
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DevTest.settings')
+    app = application
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
