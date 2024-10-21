acc6317384ff26cdc24f543a4a45f1554f3f5b69
Arju <arju234567@gmail.com>
2024-10-21 13:36:19 +0530

    Production config

--- DevTest/settings.py
+++ DevTest/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-import smtplib
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-76%!@mr_jaad&ivzfi6qatukb16c734&p)+arb()g!ahzj&ic('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '.localhost', '127.0.0.1', '[::1]'
